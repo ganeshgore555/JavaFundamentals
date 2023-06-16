5c439dda7f32db3437fdb3c18c346d39db05fe48
Ganensh Gore <ganeshgore555@gmail.com>
2023-06-16 19:02:02 +0530

    Update BitwiseOperations.java

--- src/com/main/BitwiseOperations.java
+++ src/com/main/BitwiseOperations.java
@@ -5,7 +5,14 @@ public class BitwiseOperations {
 	public static void main(String[] args) {
 		System.out.println(5^4^17^4^5);
 		int mask = 2;
-		System.out.print(Integer.toBinaryString(mask | (1 << 3)));
+		System.out.println(Integer.toBinaryString(mask | (1 << 3)));
+		
+		System.out.println(1&-1);
+		System.out.println(4&-4);
+		System.out.println(7&-7);
+		System.out.println(Integer.toBinaryString(1) + "-" + Integer.toBinaryString(-1));
+		System.out.println(Integer.toBinaryString(7) + "-" + Integer.toBinaryString(-7));
+		System.out.println(Integer.toBinaryString(4) + "-" + Integer.toBinaryString(-4));
 	}
 
 }
